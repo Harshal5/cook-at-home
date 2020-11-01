e6dedf0dea86ca100d4345fd025c2df797b9d78d
kanhaiya38 <kanhaiya38@protonmail.com>
2020-11-01 20:43:51 +0530

    added recipe list and recipes

--- backend/handlers/recipes.js
+++ backend/handlers/recipes.js
@@ -1,29 +1,28 @@
 const config = require("config");
 const db = require("../config/db");
 
-module.exports.getAllRecipes = async(req, res, next) =>{
-    try {
-        let query = `SELECT * FROM recipe`;
-        db.query(query, (err,result) => {
-            if(err) throw err;
-            console.log(result);
-            return res.status(200).json(result);
-        });
-    } catch (err) {
-        next(err)
-    }
+module.exports.getAllRecipes = async (req, res, next) => {
+	try {
+		let query = `SELECT recipe_id, name, description FROM recipe`;
+		db.query(query, (err, result) => {
+			if (err) throw err;
+			console.log(result);
+			return res.status(200).json(result);
+		});
+	} catch (err) {
+		next(err);
+	}
 };
 
-module.exports.getARecipe = async(req, res, next) =>{
-    try {
-        let query = `SELECT * FROM recipe WHERE recipe_id = ${req.params.recipe_id}`;
-        db.query(query, (err,result) => {
-            if(err) throw err;
-            console.log(result[0]);
-            return res.status(200).json(result[0]);
-        });
-    } catch (err) {
-        next(err)
-    }
+module.exports.getARecipe = async (req, res, next) => {
+	try {
+		let query = `SELECT * FROM recipe WHERE recipe_id = ${req.params.recipe_id}`;
+		db.query(query, (err, result) => {
+			if (err) throw err;
+			console.log(result[0]);
+			return res.status(200).json(result[0]);
+		});
+	} catch (err) {
+		next(err);
+	}
 };
-
