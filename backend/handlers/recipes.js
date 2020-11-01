9ac802f343329de71be5fddacd3255d6e375e8a0
Harshal5 <harshalnpatil5@gmail.com>
2020-11-01 15:39:46 +0530

    get review and ingredients route

--- backend/handlers/recipes.js
+++ backend/handlers/recipes.js
@@ -10,7 +10,7 @@ module.exports.getAllRecipes = async(req, res, next) =>{
             return res.status(200).json(result);
         });
     } catch (err) {
-        next(err)
+        next(err);
     }
 };
 
@@ -23,7 +23,32 @@ module.exports.getARecipe = async(req, res, next) =>{
             return res.status(200).json(result[0]);
         });
     } catch (err) {
-        next(err)
+        next(err);
     }
 };
 
+module.exports.getReview = async(req, res, next) => {
+    try {
+        let query = `SELECT user.name, reviews.rating, reviews.comment FROM reviews INNER JOIN user on (user.user_id = reviews.user_id) WHERE reviews.recipe_id= ${req.params.recipe_id}`;
+        db.query(query, (err, result) => {
+            if(err) throw err;
+            console.log(result);
+            return res.status(200).json(result)
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports.getIngredients = async(req, res, next) => {
+    try {
+        let query = `SELECT stock.ingredient_name, ingredients.quantity FROM ingredients INNER JOIN stock on (stock.ingredient_id = ingredients.ingredient_id) WHERE ingredients.recipe_id= ${req.params.recipe_id}`;
+        db.query(query, (err, result) => {
+            if(err) throw err;
+            console.log(result);
+            return res.status(200).json(result)
+        });
+    } catch (err) {
+        next(err);
+    }
+}
--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getAllRecipes, getARecipe } = require("../handlers/recipes")
+const { getAllRecipes, getARecipe, getReview, getIngredients } = require("../handlers/recipes")
 
 router.get("/", getAllRecipes);
 router.get("/:recipe_id", getARecipe);
+router.get("/:recipe_id/review", getReview);
+router.get("/:recipe_id/ingredients", getIngredients);
+
 
 module.exports = router;
