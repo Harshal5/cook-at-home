847cb5b181f24a6b2a982c84b236e532a576276a
Harshal5 <51528083+Harshal5@users.noreply.github.com>
2020-11-07 14:17:55 +0530

    Update order.js

--- backend/handlers/order.js
+++ backend/handlers/order.js
@@ -4,7 +4,10 @@ const db = require("../config/db");
 module.exports.makeOrder = async (req, res, next) => {
 	try {
 		const { user_id, bill } = req.body;
+		
+		// Query to place a new Order
 		const query = `INSERT INTO orders(user_id, order_time, bill) VALUES(${user_id}, NOW(), ${bill})`;
+		
 		db.query(query, (err, result) => {
 			if (err) throw err;
 			console.log(result);
@@ -17,7 +20,10 @@ module.exports.makeOrder = async (req, res, next) => {
 
 module.exports.getAllOrders = async (req, res, next) => {
 	try {
+		
+		// Query to fetch all the orders for the admin
 		const query = `SELECT name, order_id, order_time, bill FROM orders o INNER JOIN user u ON o.user_id = u.user_id`;
+		
 		db.query(query, (err, result) => {
 			if (err) throw err;
 			console.log(result);
